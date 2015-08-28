6df18d2b5fc35e798e76da8ea3226c2cd99bd6c9
Luís Gustavo Verri Zacheu <luiszacheu@gmail.com>
2015-08-28 14:57:33 -0300

    Refatorando método que verifica o tipo de conexao

--- app/src/main/java/br/com/compreingressos/utils/ConnectionUtils.java
+++ app/src/main/java/br/com/compreingressos/utils/ConnectionUtils.java
@@ -3,6 +3,8 @@ package br.com.compreingressos.utils;
 import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.telephony.TelephonyManager;
+import android.util.Log;
 
 import com.crashlytics.android.Crashlytics;
 
@@ -12,14 +14,39 @@ import com.crashlytics.android.Crashlytics;
 public class ConnectionUtils {
 
     public static String getTypeNameConnection(Context context) {
-        ConnectivityManager connManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
-
-        try {
-            return connManager.getActiveNetworkInfo().getTypeName();
-        } catch (Exception e) {
-            Crashlytics.logException(e);
-            return "";
+        ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo info = cm.getActiveNetworkInfo();
+        if(info==null || !info.isConnected())
+            return "-"; //not connected
+        if(info.getType() == ConnectivityManager.TYPE_WIFI)
+            return "WIFI";
+        if(info.getType() == ConnectivityManager.TYPE_MOBILE){
+            int networkType = info.getSubtype();
+            switch (networkType) {
+                case TelephonyManager.NETWORK_TYPE_GPRS:
+                case TelephonyManager.NETWORK_TYPE_EDGE:
+                case TelephonyManager.NETWORK_TYPE_CDMA:
+                case TelephonyManager.NETWORK_TYPE_1xRTT:
+                case TelephonyManager.NETWORK_TYPE_IDEN: //api<8 : replace by 11
+                    return "2G";
+                case TelephonyManager.NETWORK_TYPE_UMTS:
+                case TelephonyManager.NETWORK_TYPE_EVDO_0:
+                case TelephonyManager.NETWORK_TYPE_EVDO_A:
+                case TelephonyManager.NETWORK_TYPE_HSDPA:
+                case TelephonyManager.NETWORK_TYPE_HSUPA:
+                case TelephonyManager.NETWORK_TYPE_HSPA:
+                case TelephonyManager.NETWORK_TYPE_EVDO_B: //api<9 : replace by 14
+                case TelephonyManager.NETWORK_TYPE_EHRPD:  //api<11 : replace by 12
+                case TelephonyManager.NETWORK_TYPE_HSPAP:  //api<13 : replace by 15
+                    return "3G";
+                case TelephonyManager.NETWORK_TYPE_LTE:    //api<11 : replace by 13
+                    return "4G";
+                default:
+                    return "?";
+            }
         }
+
+        return "?";
     }
 
     public static boolean isInternetOn(Context context) {
